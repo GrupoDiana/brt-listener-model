21dc0a805ffc55fe2e3300da6634d7a990a7b794
Luis Molina-Tanco <lmtanco@uma.es>
2023-10-25 18:30:08 +0200

    udpate dependencies

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,12 +1,23 @@
 include(FetchContent)
 
+# Download our fork of avendish
 FetchContent_Declare(
   avendish
   GIT_REPOSITORY "https://github.com/GrupoDiana/avendish"
-  GIT_TAG  e956c2d 
+  GIT_TAG  main 
   GIT_PROGRESS true
 )
 FetchContent_Populate(avendish)
+
+# Donwload the BRT Library
+include(FetchContent)
+FetchContent_Declare(
+    brt
+    GIT_REPOSITORY "https://github.com/GrupoDiana/BRTLibrary.git"
+    GIT_TAG cmake
+    GIT_PROGRESS true
+)
+FetchContent_MakeAvailable(brt)
 
 set(CMAKE_PREFIX_PATH "${avendish_SOURCE_DIR};${CMAKE_PREFIX_PATH}")
 find_package(Avendish REQUIRED)
